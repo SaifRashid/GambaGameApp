675d081cd1db24a2732fef58daac4182a8e9b16b
SaifRashid <42101711+SaifRashid@users.noreply.github.com>
2024-01-28 23:47:52 -0500

    Refactored code

--- app/src/main/java/com/example/gambagame/MainActivity.kt
+++ app/src/main/java/com/example/gambagame/MainActivity.kt
@@ -13,99 +13,102 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    fun plusMinusPlay(view: View) {
+        var betValueInt = findViewById<TextView>(R.id.text_bet_value).text.toString().replace("$", "").toInt()
+        var balanceValueInt = findViewById<TextView>(R.id.text_balance_value).text.toString().replace("$", "").toInt()
 
-    fun play(view: View) {
-        val betValue = findViewById<TextView>(R.id.text_bet_value)
-        val balanceValue = findViewById<TextView>(R.id.text_balance_value)
-        val betValueInt = betValue.text.toString().replace("$", "").toInt()
-        var balanceValueInt = balanceValue.text.toString().replace("$", "").toInt()
+        if (view.id == R.id.button_plus) {
+            if (betValueInt < balanceValueInt) {
+                betValueInt++
+                updateBet(betValueInt)
+            }
+        } else if (view.id == R.id.button_minus) {
+                if (betValueInt > 1) {
+                    betValueInt--
+                    updateBet(betValueInt)
+            }
+        } else if (view.id == R.id.button_play) {
+            if (betValueInt <= balanceValueInt) {
+                updateSlots("random")
+
+                if (equalSlots()) {
+                    balanceValueInt += betValueInt * 5
+                    updateBalance(balanceValueInt)
+                    updateMessage("Hooray! You've won!", "visible", "#4CAF50")
+                } else {
+                    balanceValueInt -= betValueInt
+                    updateBalance(balanceValueInt)
+                    updateMessage("You lost! Try again!", "visible", "#F44336")
+                    if (balanceValueInt == 0) {
+                        updateButtons(false)
+                        updateMessage("You lost, game is over!", "visible", "#F44336")
+                    }
+                }
+            } else {
+                updateMessage("Insufficient funds for $${betValueInt} bet", "visible", "#F44336")
+            }
+        }
+    }
+    fun resetGame(view: View) {
+        updateBet(1)
+        updateBalance(10)
+        updateSlots("reset")
+        updateButtons(true)
+        updateMessage("Hello", "invisible", "#FFFFFF")
+    }
+    private fun equalSlots(): Boolean {
+        val slot1 = findViewById<TextView>(R.id.text_slot1).text.toString().toInt()
+        val slot2 = findViewById<TextView>(R.id.text_slot2).text.toString().toInt()
+        val slot3 = findViewById<TextView>(R.id.text_slot3).text.toString().toInt()
+
+        return slot1 == slot2 && slot1 == slot3
+    }
+    private fun updateSlots(string: String) {
         val slot1 = findViewById<TextView>(R.id.text_slot1)
         val slot2 = findViewById<TextView>(R.id.text_slot2)
         val slot3 = findViewById<TextView>(R.id.text_slot3)
-        val play = findViewById<Button>(R.id.button_play)
-        val plus = findViewById<Button>(R.id.button_plus)
-        val minus = findViewById<Button>(R.id.button_minus)
-        val message = findViewById<TextView>(R.id.text_feedback_message)
 
-        message.setTextColor(Color.parseColor("#F44336"))
-
-        if (betValueInt <= balanceValueInt) {
+        if (string == "random") {
             slot1.text = "${generateRandomNum()}"
             slot2.text = "${generateRandomNum()}"
             slot3.text = "${generateRandomNum()}"
-
-            if (slot1.text.toString().toInt() == slot2.text.toString().toInt() && slot1.text.toString().toInt() == slot3.text.toString().toInt()) {
-                balanceValueInt += betValueInt * 5
-                balanceValue.text = "$${balanceValueInt}"
-                message.text = "Hooray! You've won!"
-                message.setTextColor(Color.parseColor("#4CAF50"))
-            } else {
-                balanceValueInt -= betValueInt
-                balanceValue.text = "$${balanceValueInt}"
-                message.text = "You lost! Try again!"
-                if (balanceValueInt == 0) {
-                    play.isEnabled = false
-                    plus.isEnabled = false
-                    minus.isEnabled = false
-                    message.text = "You lost, game is over!"
-                }
-            }
-
-        } else {
-            message.text = "Insufficient funds for $${betValueInt} bet"
-        }
-
-        message.visibility = View.VISIBLE
-    }
-
-    fun plus(view: View) {
-        val betValue = findViewById<TextView>(R.id.text_bet_value)
-        val balanceValue = findViewById<TextView>(R.id.text_balance_value)
-        var betValueInt = betValue.text.toString().replace("$", "").toInt()
-        val balanceValueInt = balanceValue.text.toString().replace("$", "").toInt()
-
-        if (betValueInt < balanceValueInt) {
-            betValueInt++
-            betValue.text = "$${betValueInt}"
+        } else if (string == "reset") {
+            slot1.text = "1"
+            slot2.text = "2"
+            slot3.text = "3"
         }
     }
+    private fun updateMessage(text: String, visibility: String, color: String) {
+        val message = findViewById<TextView>(R.id.text_feedback_message)
 
-    fun minus(view: View) {
-        val betValue = findViewById<TextView>(R.id.text_bet_value)
-        val balanceValue = findViewById<TextView>(R.id.text_balance_value)
-        var betValueInt = betValue.text.toString().replace("$", "").toInt()
-        var balanceValueInt = balanceValue.text.toString().replace("$", "").toInt()
-
-        if (betValueInt > 1) {
-            betValueInt--
-            betValue.text = "$${betValueInt}"
+        message.text = text
+        message.setTextColor(Color.parseColor(color))
+        message.visibility = when (visibility) {
+            "visible" -> View.VISIBLE
+            "invisible" -> View.INVISIBLE
+            else -> View.VISIBLE
         }
     }
-
-    fun resetGame(view: View) {
-        val betValue = findViewById<TextView>(R.id.text_bet_value)
-        val balanceValue = findViewById<TextView>(R.id.text_balance_value)
-        val slot1 = findViewById<TextView>(R.id.text_slot1)
-        val slot2 = findViewById<TextView>(R.id.text_slot2)
-        val slot3 = findViewById<TextView>(R.id.text_slot3)
+    private fun updateButtons(boolean: Boolean) {
         val play = findViewById<Button>(R.id.button_play)
         val plus = findViewById<Button>(R.id.button_plus)
         val minus = findViewById<Button>(R.id.button_minus)
-        val message = findViewById<TextView>(R.id.text_feedback_message)
 
-        betValue.text = "$1"
-        balanceValue.text = "$10"
-        slot1.text = "1"
-        slot2.text = "2"
-        slot3.text = "3"
-        play.isEnabled = true
-        plus.isEnabled = true
-        minus.isEnabled = true
-        message.visibility = View.INVISIBLE
+        play.isEnabled = boolean
+        plus.isEnabled = boolean
+        minus.isEnabled = boolean
     }
+    private fun updateBalance(value: Int) {
+        val balanceValue = findViewById<TextView>(R.id.text_balance_value)
 
+        balanceValue.text = "$${value}"
+    }
+    private fun updateBet(value: Int) {
+        val betValue = findViewById<TextView>(R.id.text_bet_value)
+
+        betValue.text = "$${value}"
+    }
     private fun generateRandomNum(): Int {
         return Random().nextInt(9) + 1
     }
-
 }
